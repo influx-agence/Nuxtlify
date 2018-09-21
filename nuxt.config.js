027b06eea9294de56ffecc392c4f70f2810d5d67
influx-agence <43016695+influx-agence@users.noreply.github.com>
2018-09-21 12:22:16 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,4 @@
 const webpack = require("webpack")
-const glob = require('glob');
-const path = require('path');
-
-// Enhance Nuxt's generate process by gathering all content files from Netifly CMS
-// automatically and match it to the path of your Nuxt routes.
-// The Nuxt routes are generate by Nuxt automatically based on the pages folder.
-const dynamicRoutes = getDynamicPaths({
-  '/blog': 'blog/posts/*.json'
-});
 module.exports = {
   modules: ["nuxtent", "nuxt-netlify-cms"],
   mode: "universal",
@@ -61,18 +52,4 @@ module.exports = {
       }
     }
   }
-}
-/**
- * Create an array of URLs from a list of files
- * @param {*} urlFilepathTable
- */
-function getDynamicPaths(urlFilepathTable) {
-  return [].concat(
-    ...Object.keys(urlFilepathTable).map(url => {
-      var filepathGlob = urlFilepathTable[url];
-      return glob
-        .sync(filepathGlob, { cwd: 'content' })
-        .map(filepath => `${url}/${path.basename(filepath, '.json')}`);
-    })
-  );
 }
