040a866a2f6d47b0b527b9d40e52bf8770d60895
influx-agence <43016695+influx-agence@users.noreply.github.com>
2018-09-27 12:11:48 +0200

    Update _page.vue

--- pages/blog/_page.vue
+++ pages/blog/_page.vue
@@ -7,16 +7,10 @@
 
 <script>
     export default {
-      data() {
-        return {
-          post: 0
-        };
-      },
       async asyncData({ app, params }) {
         let post = await import('~/content/posts/' + params.slug + '.json');
         return post;
       },
-
       head() {
         return {
           title: `${this.post.title}`,
